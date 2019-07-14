11a05d72e71b917792f66f902721dc5e73cf884d
Terbau <40792825+Terbau@users.noreply.github.com>
2019-07-15 01:48:13 +0200

    Update version

--- fortnitepy/__init__.py
+++ fortnitepy/__init__.py
@@ -24,7 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 from .client import Client
 from .friend import Friend, PendingFriend
