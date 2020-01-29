2442d3c28c1702626c9eb41c12a7450e162c1df5
Oli <55298769+xMistt@users.noreply.github.com>
2020-01-29 20:27:07 +0000

    Update discord_intergration.py

--- examples/discord_intergration.py
+++ examples/discord_intergration.py
@@ -1,5 +1,24 @@
 import fortnitepy
 from discord.ext import commands
+import json
+import os
+
+email = 'email@email.com'
+password = 'password1'
+filename = 'device_auths.json'
+
+def get_device_auth_details():
+    if os.path.isfile(filename):
+        with open(filename, 'r') as fp:
+            return json.load(fp)
+    return {}
+
+def store_device_auth_details(email, details):
+    existing = get_device_auth_details()
+    existing[email] = details
+
+    with open(filename, 'w') as fp:
+        json.dump(existing, fp)
 
 discord_bot = commands.Bot(
     command_prefix='!',
@@ -7,9 +26,15 @@ discord_bot = commands.Bot(
     case_insensitive=True
 )
 
+device_auth_details = get_device_auth_details().get(email, {})
 fortnite_client = fortnitepy.Client(
-    email='email',
-    password='password',
+    auth=fortnitepy.AdvancedAuth(
+        email=email,
+        password=password,
+        prompt_exchange_code=True,
+        delete_existing_device_auths=True,
+        **device_auth_details
+    ),
     loop=discord_bot.loop
 )
 
@@ -18,6 +43,10 @@ async def on_ready():
     print('Discord bot ready')
     await fortnite_client.start()
 
+@fortnite_client.event
+async def event_device_auth_generate(details, email):
+    store_device_auth_details(email, details)
+    
 @fortnite_client.event
 async def event_ready():
     print('Fortnite client ready')
