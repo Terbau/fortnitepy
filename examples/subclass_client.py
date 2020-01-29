3b1b1333378aa914acedfdf4167c100799ee6af4
Oli <55298769+xMistt@users.noreply.github.com>
2020-01-29 20:29:48 +0000

    Update subclass_client.py

--- examples/subclass_client.py
+++ examples/subclass_client.py
@@ -1,11 +1,39 @@
 import fortnitepy
+import json
+import os
+
+email = 'email@email.com'
+password = 'password1'
+filename = 'device_auths.json'
+
+def get_device_auth_details():
+    if os.path.isfile(filename):
+        with open(filename, 'r') as fp:
+            return json.load(fp)
+    return {}
+
+def store_device_auth_details(email, details):
+    existing = get_device_auth_details()
+    existing[email] = details
+
+    with open(filename, 'w') as fp:
+        json.dump(existing, fp)
 
 class MyClient(fortnitepy.Client):
     def __init__(self):
+        device_auth_details = get_device_auth_details().get(email, {})
         super().__init__(
-            email='example@email.com',
-            password='password123'
+            auth=fortnitepy.AdvancedAuth(
+                email=email,
+                password=password,
+                prompt_exchange_code=True,
+                delete_existing_device_auths=True,
+                **device_auth_details
+            )
         )
+        
+    async def event_device_auth_generate(self, details, email):
+        store_device_auth_details(email, details)
     
     async def event_ready(self):
         print('----------------')
