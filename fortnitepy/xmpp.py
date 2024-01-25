4cbb4d1b1e31ca538916840e10a419c5f0cd6071
kam <71592833+PirxcyFinal@users.noreply.github.com>
2024-01-25 19:50:11 +0000

    Update xmpp.py

--- fortnitepy/xmpp.py
+++ fortnitepy/xmpp.py
@@ -1120,7 +1120,7 @@ class XMPPClient:
         print(party.me.leader)
         if updatedState.get("Default:SuggestedLink_j") and party.me.leader:
             SuggestedLink = updatedState['Default:SuggestedLink_j']
-            await party.set_playlist(SuggestedLink['mnemonic']_
+            await party.set_playlist(SuggestedLink['mnemonic'])
             
         if party._default_config.team_change_allowed or not party.me.leader:
             req_j = body['member_state_updated'].get(
